6621f0c199e0d2a8d0a14fedaae7dd53cc14c802
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2019-06-13 18:15:40 +0200

    add doctype

--- changelog.php
+++ changelog.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name=robots content="noindex, nofollow"/>
+<title>Chengelogs Reader at ghsvs.de</title>
+</head>
+<body>
 <?php
 /*
  * Call via https://updates.ghsvs.de/changelog.php?file=justtesting&element=whatever
@@ -101,4 +109,7 @@ foreach ($data as $key => $changelog)
 	echo '<hr><h3>Version ' . $key . '</h3>';
 	echo implode("\n", $changelog) . '<hr>';
 }
-return;
+
+?>
+</body>
+</html>
